42408473a14cc18d44d1f079d25542cdb4cbaba4
Deniss Kaibagarovs <deniss.kaibagarovs@paymentsense.com>
2023-05-26 14:27:08 +0100

    VT checkout screen extracted

--- Sources/dojo-ios-sdk-drop-in-ui/Classes/DojoSDKDropInUI.swift
+++ Sources/dojo-ios-sdk-drop-in-ui/Classes/DojoSDKDropInUI.swift
@@ -42,6 +42,43 @@ public class DojoSDKDropInUI: NSObject {
             }
         }
     }
+    
+    @objc
+    public func getVTCheckout(paymentIntentId: String,
+                              debugConfig: DojoSDKDebugConfig? = nil,
+                              completion: ((UIViewController?) -> Void)?) {
+        let dataLoadingModel = DataLoadingViewModel(paymentIntentId: paymentIntentId,
+                                                    debugConfig: debugConfig,
+                                                    demoDelay: 0,
+                                                    isDemo: false)
+        dataLoadingModel.fetchPaymentIntent() { pi, error in
+            if let pi = pi {
+                var themeSettings = DojoThemeSettings.getLightTheme()
+                themeSettings.showBranding = false
+                let configManager = ConfigurationManager(paymentIntentId: paymentIntentId, paymentIntent: pi, themeSettings: ThemeSettings(dojoTheme: themeSettings))
+                if let viewModel = CardDetailsCheckoutViewModel(config: configManager) {
+                    let checkoutController = CardDetailsCheckoutViewController(viewModel: viewModel, theme: configManager.themeSettings, delegate: self)
+                    completion?(checkoutController)
+                } else {
+                    completion?(nil)
+                    // TODO return
+                }
+            } else {
+                // TODO return
+                completion?(nil)
+            }
+        }
+    }
+}
+
+extension DojoSDKDropInUI: CardDetailsCheckoutViewControllerDelegate {
+    func onForceClosePress() {
+        
+    }
+    
+    func navigateToPaymentResult(resultCode: Int) {
+        
+    }
 }
 
 @objc
