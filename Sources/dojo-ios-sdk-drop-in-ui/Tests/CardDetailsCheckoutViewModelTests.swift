69e2b0ac8ee57f2268d1c654c5232d5472afb479
Deniss Kaibagarovs <deniss.kaibagarovs@paymentsense.com>
2024-02-22 14:22:33 +0000

    fix test

--- Sources/dojo-ios-sdk-drop-in-ui/Tests/CardDetailsCheckoutViewModelTests.swift
+++ Sources/dojo-ios-sdk-drop-in-ui/Tests/CardDetailsCheckoutViewModelTests.swift
@@ -5,7 +5,7 @@ class CardDetailsCheckoutViewModelTests: XCTestCase {
     
     func testBillingPostcode() {
         let viewModel = CardDetailsCheckoutViewModel(config: ConfigurationManager(paymentIntentId: "",
-                                                                                  paymentIntent: PaymentIntent(id: "", clientSessionSecret: "", amount: DojoPaymentIntentAmount(value: 10, currencyCode: "GBP")),
+                                                                                  paymentIntent: PaymentIntent(id: "", clientSessionSecret: "", totalAmount: DojoPaymentIntentAmount(value: 10, currencyCode: "GBP")),
                                                                                   themeSettings: ThemeSettings(dojoTheme: DojoThemeSettings.getLightTheme())))!
         XCTAssertTrue(viewModel.showBillingPostcode("GB"))
     }
