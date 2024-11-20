e169de6814a78448ede4167b9e7132638ac5a4f8
Deniss Kaibagarovs <deniss.kaibagarovs@paymentsense.com>
2024-11-20 09:36:54 +0000

    bump code-sdk version

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     dependencies: [
         // Dojo libs
-        .package(url: "git@github.com:dojo-engineering/dojo-ios-sdk", from: "1.4.2"),
+        .package(url: "git@github.com:dojo-engineering/dojo-ios-sdk", from: "1.4.3"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
